33e2e500feeb5ebac16b2ef3b8bdb5aa49616d02
ArthurApf <99188046+ArthurApf@users.noreply.github.com>
2022-10-06 16:57:58 -0300

    aasd

---
+++ Semana 5/Exercício de personalização da Etapa 5/Curriculo_sem5.js
@@ -0,0 +1,23 @@
+  const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end("Deu green");
+});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); 
+
+function disc(){
+  let w = document.getElementById("word")
+  w.innerHTML="Esse é meu curriculo, para contato cel:(11)999149848"
+}
+
+function some(){
+  let a = document.getElementById("foto")
+  $(a).fadeOut("500000000");
+}
+
+
