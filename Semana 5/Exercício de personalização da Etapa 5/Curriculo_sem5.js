a0a0716d75257d3efcff99748c0944b0c41df75e
ArthurApf <arthur.fraige@sou.inteli.edu.br>
2022-10-06 23:17:50 -0300

    asdasd

--- Semana 5/Exercício de personalização da Etapa 5/Curriculo_sem5.js
+++ Semana 5/Exercício de personalização da Etapa 5/Curriculo_sem5.js
@@ -1,23 +1,31 @@
-  const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end("Deu green");
-});
-server.listen(port, hostname, () => {
+import express from 'express';
+const sqlite3 = require("sqlite3");
+const app = express();
+const hostname = "127.0.0.1";
+const port = 5500;
+const db = new sqlite3.Database("info_curr.db")
+const cors = require("cors");
+const path = require("path");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function(req,res){
+  res.sendFile(
+    path.resolve(__dirname + "/Curr_sem5.html")
+  )
+})
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-}); 
-
-function disc(){
-  let w = document.getElementById("word")
-  w.innerHTML="Esse é meu curriculo, para contato cel:(11)999149848"
-}
-
-function some(){
-  let a = document.getElementById("foto")
-  $(a).fadeOut("500000000");
-}
+});
 
 
+app.get("/end1", (req, res) => {
+  db.get(
+    `SELECT INFO FROM skills`,
+    (error, data) => {
+      res.json(data)
+    }
+  );
+});
